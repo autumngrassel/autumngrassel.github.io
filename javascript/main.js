6341351c0ac2bbdc2ff5798b47b7aff6128b186c
AUTUMN GRASSEL <grassela@uw.edu>
2016-03-07 13:12:06 -0800

    added advance button

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,4 @@
+
 // Initialize tooltip
 tip = d3.tip()
   .attr('class', 'tooltip')
@@ -24,6 +25,8 @@ var svg = d3.select("#main").append("svg")
 	.attr("height", 400)
 	.attr("id", "svg_main")
 	.attr("border", 1);
+
+drawBacteria();
 
 svg.call(tip);
 
@@ -68,31 +71,33 @@ var MouseOut = function(object) {
   .attr("rx", 5);
 }
 
-// populate svg with bacteria placed randomly
-for (var i = 0; i <= 100; i++) {
-	//Math.random(); // returns between 0 and 1
-	var x = Math.floor(Math.random() * (780)) + 0;// + xStart;
-	var y = Math.floor(Math.random() * (380)) + 10; // + yStart;
-	var rotate = Math.floor(Math.random() * 90);
-	if (Math.random() < 0.5) {
-		rotate = -1 * rotate;
+function drawBacteria() {
+	// populate svg with bacteria placed randomly
+	for (var i = 0; i <= 200; i++) {
+		//Math.random(); // returns between 0 and 1
+		var x = Math.floor(Math.random() * (780)) + 0;// + xStart;
+		var y = Math.floor(Math.random() * (380)) + 10; // + yStart;
+		var rotate = Math.floor(Math.random() * 90);
+		if (Math.random() < 0.5) {
+			rotate = -1 * rotate;
+		}
+		svg.append("rect")         // attach a rectangle
+	      .attr("class", "bacteria")
+		    .attr("x", x)          // position the left of the rectangle
+		    .attr("y", y)          // position the top of the rectangle
+		    .attr("height", 10)    // set the height
+		    .attr("width", 20)     // set the width
+		    .attr("rx", 5)         // set the x corner curve radius
+		    .attr("fill", "purple")
+	      .attr("opacity", 0.7)
+
+				.on("mouseover", MouseOver )
+	    	.on("mouseout", MouseOut)
+
+		    .attr("transform", "rotate(" + rotate + " " + (x + 10 ) + " " + (y + 5) + ")");
+	      // set the y corner curve radius
+		    // if you don't have the rotation, they're all in the frame
+		    //.attr("transform", "rotate(" + rotate + ")");        // set the y corner curve radius
+
 	}
-	svg.append("rect")         // attach a rectangle
-      .attr("class", "bacteria")
-	    .attr("x", x)          // position the left of the rectangle
-	    .attr("y", y)          // position the top of the rectangle
-	    .attr("height", 10)    // set the height
-	    .attr("width", 20)     // set the width
-	    .attr("rx", 5)         // set the x corner curve radius
-	    .attr("fill", "purple")
-      .attr("opacity", 0.7)
-
-			.on("mouseover", MouseOver )
-    	.on("mouseout", MouseOut)
-
-	    .attr("transform", "rotate(" + rotate + " " + (x + 10 ) + " " + (y + 5) + ")");
-      // set the y corner curve radius
-	    // if you don't have the rotation, they're all in the frame
-	    //.attr("transform", "rotate(" + rotate + ")");        // set the y corner curve radius
-
 }
