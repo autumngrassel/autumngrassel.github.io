6c546da0c12eef77a2a0e965971eee424cd69b03
MadisonIkeda <maikeda@uw.edu>
2016-03-15 20:33:34 -0700

    box style

--- javascript/explanations.js
+++ javascript/explanations.js
@@ -17,5 +17,9 @@ $(document).ready(function() {
     d3.selectAll('#definitionsHack')
       .style('opacity',  function(d,i) { return 0.0; });
 
+    d3.selectAll('#accomplishes')
+      .style('opacity',  function(d,i) { return i == index ? 0.3 : 0.5; });
+    d3.selectAll('#inner-accomplishments')
+      .style('opacity',  function(d,i) { return i == index ? 1 : 0.5; });
   });
 });
